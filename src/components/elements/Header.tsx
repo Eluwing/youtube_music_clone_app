99b1c1d4d1a4f8e5eee4ed4d2a13b4816d7525a5
Eluwing <abek0403@gmail.com>
2025-04-04 16:45:07 +0900

    feat:ヘッダーコンポーネントスクロールレスポンシブ対応

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import UserIcon from "./UserIcon";
 import PagePadding from "./PagePadding";
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/drawer";
 import Logo from "./Logo";
 import Navigator from "./Navigator";
+import { cn } from "@/lib/utils";
 
 const HeaderDrawer = ({children}: {children: React.ReactNode}) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -32,8 +33,23 @@ const HeaderDrawer = ({children}: {children: React.ReactNode}) => {
 };
 
 const Header = ({ children }: { children: React.ReactNode }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const headerRef = useRef<HTMLElement | null>(null);
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      const scrollValue = headerRef?.current?.scrollTop;
+      setIsScrolled(scrollValue !== 0);
+    };
+
+    headerRef?.current?.addEventListener("scroll", handleScroll);
+    
+    return () => {
+      headerRef?.current?.addEventListener("scroll", handleScroll);
+    }
+  },[]);
   return (
-    <header className="relative overflow-y-auto w-full h-full">
+    <header ref={headerRef} className="relative overflow-y-auto w-full h-full">
       <section className="absolute top-0 w-full">
         <div className="relative h-[400px] w-full">
           <Image
@@ -46,12 +62,12 @@ const Header = ({ children }: { children: React.ReactNode }) => {
           <div className="absolute h-[400px] top-0 bg-gradient-to-t from-black w-full"></div>
         </div>
       </section>
-      <section className="sticky">
+      <section className={cn("sticky top-0 left-0 z-10", isScrolled && "bg-black")}>
         <PagePadding>
           <div className="h-[64px] flex flex-row justify-between items-center">
             <article
               className="h-[42px] min-w-[480px] hidden lg:flex flex-row items-center
-            bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px]"
+            bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px] border border-neutral-500"
             >
               <div>
                 <FiSearch size={24} />
